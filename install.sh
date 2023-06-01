cdf1e1a6dfd090a4afcc963376ca7d6555551579
Michael Wetter <mwetter@lbl.gov>
2023-06-01 05:58:23 -0700

    Changed sha of Buildings

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
-rm -rf Buildings-v9.1.0.zip Buildings\ 9.1.0
+sha="ee358f37a66144a64650b9a0ff13e003136ca1fd"
+rm -rf ${sha}.zip modelica-buildings-${sha}
 rm -rf buildingspy-4.0.0.tar.gz buildingspy-4.0.0
 
-wget https://github.com/lbl-srg/modelica-buildings/releases/download/v9.1.0/Buildings-v9.1.0.zip
-unzip Buildings-v9.1.0.zip
-rm -rf Buildings-v9.1.0.zip
-export MODELICAPATH=`pwd`
+wget https://github.com/lbl-srg/modelica-buildings/archive/${sha}.zip
+unzip ${sha}.zip
+rm -rf ${sha}.zip
+mv modelica-buildings-${sha}/Buildings Buildings\ 9.1.1
+rm -rf modelica-buildings-${sha}
 
 
 wget https://simulationresearch.lbl.gov/modelica/releases/python/buildingspy-4.0.0.tar.gz
 tar xzf buildingspy-4.0.0.tar.gz
 rm -rf buildingspy-4.0.0.tar.gz
-export PYTHONPATH=`pwd`/buildingspy-4.0.0:${PYTHONPATH}
