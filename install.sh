810f175e7539ad3285bf9f7d41cfd519ccbbef3f
Michael Wetter <mwetter@lbl.gov>
2023-06-02 14:33:56 -0700

    Changed to Buildings 10.0.0 dev

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-sha="ee358f37a66144a64650b9a0ff13e003136ca1fd"
+sha="444f378049ddc0cdc63f0978194a4786d139fdd0"
 rm -rf ${sha}.zip modelica-buildings-${sha}
 rm -rf buildingspy-4.0.0.tar.gz buildingspy-4.0.0
 
 wget https://github.com/lbl-srg/modelica-buildings/archive/${sha}.zip
 unzip ${sha}.zip
 rm -rf ${sha}.zip
-mv modelica-buildings-${sha}/Buildings Buildings\ 9.1.1
+mv modelica-buildings-${sha}/Buildings Buildings\ 10.0.0
 rm -rf modelica-buildings-${sha}
 
 
